140c1ef8918a25593d3bf093cea6b2f19160282a
yuzuru imai <yuzuruimai0121@gmail.com>
2023-10-03 22:07:54 +0900

    入力シート実装とプラスボタン、チェックボタンの修正

--- app/src/main/java/com/example/todolist/ui/main/MainActivity.java
+++ app/src/main/java/com/example/todolist/ui/main/MainActivity.java
@@ -7,10 +7,16 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.todolist.R;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -22,7 +28,7 @@ import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener {
+    implements NavigationView.OnNavigationItemSelectedListener, View.OnKeyListener {
 
     BottomSheetBehavior mBottomSheetBehavior;
 
@@ -30,27 +36,43 @@ public class MainActivity extends AppCompatActivity
 
     private MainViewModel mainViewModel;
 
+    private EditText listNameEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listNameEditText = findViewById(R.id.list_name_edit_text);
+        listNameEditText.setOnKeyListener(this);
+
         // ViewModelの生成
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        //fab
+        // プラスボタン
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
-            Log.d(TAG, "fab Selected!");
 
             //bottomSheet
             View bottomView = findViewById(R.id.bottom_sheet_layout);
             mBottomSheetBehavior = BottomSheetBehavior.from(bottomView);
 
-            if ( mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED ){
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-            }else{
+            if ( mBottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED ){
                 mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        // チェックボタン
+        FloatingActionButton checkBtn = findViewById(R.id.check_btn);
+        checkBtn.setOnClickListener(view -> {
+            String listName = listNameEditText.getText().toString();
+            if (listName.isEmpty()) {
+                // チェックボタン押下時にリスト名が未入力の場合は警告ダイアログの表示。
+                Toast.makeText(this, R.string.input_list_name, Toast.LENGTH_SHORT).show();
+            } else {
+                // チェックボタンを押下で、DBにリスト名追加。
+                mainViewModel.addNewTodoSheet(listName);
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
             }
         });
 
@@ -75,12 +97,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onResume(){
-        super.onResume();
-        mainViewModel.addNewTodoSheet("サンプル1");
-    }
-
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         if (R.id.menu_item1 == item.getItemId()) {
             Log.d(TAG, "menu_item1 Selected!");
@@ -97,4 +113,18 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    //Enterkeyでキーボードが消える処理
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+    // Enterkeyが押されたかを判定。
+        if (event.getAction() == KeyEvent.ACTION_DOWN
+                && keyCode == KeyEvent.KEYCODE_ENTER){
+            //ソフトキーボードを閉じる
+            InputMethodManager inputMethodManager =
+                    (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            return false;
+        }
+        return false;
+    }
 }
